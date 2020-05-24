e44e1c58a090f1932cfa417247534bfc80db6959
Alexander Kern <alexgit@alexkern.de>
2020-05-24 16:38:20 +0200

    So 24. Mai 16:38:20 CEST 2020

--- reta.py
+++ reta.py
@@ -1,36 +1,38 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import csv
+import pyphen
 
+dic = pyphen.Pyphen(lang='de_DE')
 
-def wortumbruch(text,maxlen):
-    text = text.split()
-    wordLens = 0
-    wordAmountPerLine = 0
-    newtext3 = []
-    newtext4 = ''
-    for i,word in enumerate(text):
-        wordLens += 1 + len(word)
-        wordAmountPerLine += 1
-        if wordLens > maxlen:
-            if wordAmountPerLine == 1:
-                restword = word
-                while restword != word and len(restword) > maxlen:
-                    newtext3 += [restword[:maxlen]]
-                    newtext3 += ['\n']
-                    restword = word[maxlen:]
-                if wordAmountPerLine == 1 and len(restword) <= maxlen:
-                    newtext3 += [restword[maxlen:]]
-            else:
-                newtext3 += ['\n']
-                newtext3 += [word]
-            wordLens = 0
-            wordAmountPerLine = 0
-        else:
-            newtext3 += [word]
-        newtext4 += '_'.join(newtext3) + '_'
-        newtext3 = []
-    return str(newtext4)
+#def wortumbruch(text,maxlen):
+#    text = text.split()
+#    wordLens = 0
+#    wordAmountPerLine = 0
+#    newtext3 = []
+#    newtext4 = ''
+#    for i,word in enumerate(text):
+#        wordLens += 1 + len(word)
+#        wordAmountPerLine += 1
+#        if wordLens > maxlen:
+#            if wordAmountPerLine == 1:
+#                restword = word
+#                while restword != word and len(restword) > maxlen:
+#                    newtext3 += [restword[:maxlen]]
+#                    newtext3 += ['\n']
+#                    restword = word[maxlen:]
+#                if wordAmountPerLine == 1 and len(restword) <= maxlen:
+#                    newtext3 += [restword[maxlen:]]
+#            else:
+#                newtext3 += ['\n']
+#                newtext3 += [word]
+#            wordLens = 0
+#            wordAmountPerLine = 0
+#        else:
+#            newtext3 += [word]
+#        newtext4 += '_'.join(newtext3) + '_'
+#        newtext3 = []
+#    return str(newtext4)
 
 if True:
     with open('religion.csv', mode='r') as csv_file:
@@ -39,8 +41,17 @@ if True:
             relitable += [row]
     for line in relitable:
         for cell in line:
-            cell2 = wortumbruch(cell,4)
-            print(cell2+'\n')
+            cell2 = dic.wrap(cell, 21)
+            if len(cell2) > 1:
+                second = dic.wrap(cell2[1], 21)
+                if not second is None:
+                    cell2 = (cell2[0],) + dic.wrap(cell2[1], 21)
+                else:
+                    cell2 = (cell2[0],) + (cell2[1],)
+                print(str(cell2))
+                #if not cell2 == None:
+                #    for cell3 in cell2:
+                #        print(cell3+'\n')
 
 
 
