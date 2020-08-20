485abe6d5b66a7a5ca198fda3d0378682beede84
Alexander Kern <alexgit@alexkern.de>
2020-08-20 23:29:32 +0200

    Do 20. Aug 23:29:32 CEST 2020

--- reta.py
+++ reta.py
@@ -1867,7 +1867,7 @@ class Program:
                         "transzendentalien" + neg,
                         "strukturalien" + neg,
                     ]:
-                        rowsAsNumbers |= {5, 54, 55}
+                        rowsAsNumbers |= {5, 54, 55, 65}
                     elif arg[2:15] in ["menschliches="]:
                         for thing in arg[(arg.find("=") + 1) :].split(","):
                             if thing in [neg + "liebe", neg + "ethik"]:
